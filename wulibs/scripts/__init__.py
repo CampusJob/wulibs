e08909ffa3c65ec6662338dd23849cc40bef6734
Olivier Pilotte <opilotte@wayup.com>
2020-11-09 11:44:01 -0500

    fixing imports

--- wulibs/scripts/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from .create_dblab_pr_db import create_dblab_pr_db
-from .delete_dblab_pr_db import delete_dblab_pr_db
--- wulibs/scripts/recreate_dblab_pr_db.py
+++ wulibs/scripts/recreate_dblab_pr_db.py
@@ -1,10 +1,25 @@
 
 import argparse
 import logging
+import os
 import sys
 
-import create_dblab_pr_db
-import delete_dblab_pr_db
+from wulibs.scripts import create_dblab_pr_db
+from wulibs.scripts import delete_dblab_pr_db
+
+
+NAMESERVER = os.getenv("NAMESERVER", "100.64.0.10")
+
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME", "dblab")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "dblab")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "tcjpr")
+
+KUBERNETES_NAMESPACE = os.getenv("KUBERNETES_NAMESPACE", "default")
+
+DBLAB_ENDPOINT = os.getenv("DBLAB_ENDPOINT", "10.0.61.195")
+DBLAB_URL = os.getenv("DBLAB_URL", "http://dblab.default.svc.cluster.local")
+DBLAB_TOKEN = os.getenv("DBLAB_TOKEN", "secret_token")
+DBLAB_SNAPSHOT_NAME = os.getenv("DBLAB_SNAPSHOT_NAME", "dblab_pool@initdb")
 
 
 def recreate_dblab_pr_db(pr_number):
@@ -12,8 +27,8 @@ def recreate_dblab_pr_db(pr_number):
 
     logging.info(f"recreating PR database [{database_id}]")
 
-    delete_dblab_pr_db.delete_dblab_pr_db(pr_number)
-    create_dblab_pr_db.create_dblab_pr_db(pr_number)
+    delete_dblab_pr_db(pr_number)
+    create_dblab_pr_db(pr_number)
 
 
 if __name__ == "__main__":
